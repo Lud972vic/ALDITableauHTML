0fbc7f72ca6e2d41d38dae6a93c3c9d80326777a
Lud972vic <e.ludovic@free.fr>
2023-08-01 22:25:49 +0200

    first commit

---
+++ js/datasource.js
@@ -0,0 +1,10 @@
+let greeting = document.querySelector('.greeting');
+
+// Get HTML content
+// returns "<p>Hello world!</p>"
+let html = greeting.innerHTML;
+
+// Set HTML content
+// This replaces what was in there already
+greeting.innerHTML = '<tr><td>elit@icloud.edu</td><td>(443) 697-2153</td><td>Heredia</td><td>Mexico</td><td>Dalton Garcia</td></tr><tr><td>elit@icloud.edu</td><td>(443) 697-2153</td><td>Heredia</td><td>Mexico</td><td>Dalton Garcia</td></tr>';
+
